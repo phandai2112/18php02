d9cfb70229e49c33bb2bdeddd9fbf0e8b68c30a9
Phan Dai <phandai2112@gmail.com>
2018-04-10 21:52:09 +0700

    update javascript

---
+++ Javascript/js/main.js
@@ -0,0 +1,19 @@
+var userName = "Phan Đại";
+var myPhone = "0922344285";
+var mybirthday = "01/02/93";
+var homeTown = "Nghệ An";
+document.write("<h3> Bài Tập Số 1 <h3>"+"- Họ và Tên : " + userName + "<br>" + "- Số Điện Thoại : " +myPhone +"<br>" +"- Ngày Sinh :" + mybirthday + "<br>" +"- Quê Quán :" + homeTown+"<br>");
+// Hết bài tập 1
+
+
+for (var i = 0; i<=100; i++) {
+	
+	if (i/3 === 1 ) {
+		document.write("Số chia hết cho 3 là " + "BA<br>");
+	} else if (i/5 === 1){
+		document.write("Số chia hết cho 5 là " + "NĂM<br>");
+	}else if (i/15 === 1) {
+		document.write("Số chia hết cho 15 là " + "MƯỜI LĂM<br>");
+	}
+}
+// Hết bài 2
