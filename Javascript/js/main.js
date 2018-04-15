f4a282e3990da4c9b327418dfbad5fcd06884bd4
Phan Dai <phandai2112@gmail.com>
2018-04-15 14:06:34 +0700

    update

--- Javascript/js/main.js
+++ Javascript/js/main.js
@@ -2,18 +2,18 @@ var userName = "Phan Đại";
 var myPhone = "0922344285";
 var mybirthday = "01/02/93";
 var homeTown = "Nghệ An";
-document.write("<h3> Bài Tập Số 1 <h3>"+"- Họ và Tên : " + userName + "<br>" + "- Số Điện Thoại : " +myPhone +"<br>" +"- Ngày Sinh :" + mybirthday + "<br>" +"- Quê Quán :" + homeTown+"<br>");
+document.write("<h3> Bài Tập Số 1 <h3>" + "- Họ và Tên : " + userName + "<br>" + "- Số Điện Thoại : " +myPhone +"<br>" +"- Ngày Sinh :" + mybirthday + "<br>" +"- Quê Quán :" + homeTown+"<br>");
 // Hết bài tập 1
 
 
 for (var i = 0; i<=100; i++) {
-	
-	if (i/3 === 1 ) {
-		document.write("Số chia hết cho 3 là " + "BA<br>");
-	} else if (i/5 === 1){
-		document.write("Số chia hết cho 5 là " + "NĂM<br>");
-	}else if (i/15 === 1) {
-		document.write("Số chia hết cho 15 là " + "MƯỜI LĂM<br>");
+	if (i%15 == 0) {
+		document.write("Số chia hết cho Mười Lăm là " + i +"<br>");
+	}else if(i%5 == 0 && i%3 !== 0 && i%15 !== 0) {
+		document.write("Số chia hết cho Năm là " + i +"<br>");
+	}else if(i%3 == 0){
+		document.write("Số chia hết cho Ba là " + i +"<br>");
 	}
 }
+
 // Hết bài 2
