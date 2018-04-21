3617f47010fd057f7b132510f521a54099172502
Phan Dai <phandai2112@gmail.com>
2018-04-22 00:37:43 +0700

    bai tap 22/4

---
+++ session10/vendor/js/app.js
@@ -0,0 +1,42 @@
+/*
+	imagesSex : Biến lấy đối tượng giới tính
+	userName  : Biến lấy đối tượng của họ tên
+	yearBirth : Biến lấy đối tượng ngày sinh
+	imagesAvatar : Biến lấy đối tượng ảnh
+	--> Create by : Phan Đại
+	*/
+
+	// document DOM ready
+document.addEventListener("DOMContentLoaded",function(){
+
+	var imagesSex = document.getElementsByClassName("sex");
+	var userName = document.getElementById("name");
+	var yearBirth = document.getElementById("yearBirth");
+	var imagesAvatar = document.getElementById("imagesAvatar");
+
+// Hàm kiểm tra điều kiện ngày sinh
+
+yearBirth.onchange = function(){
+	console.log(yearBirth.value);
+
+	if(yearBirth.value > 1997){
+		userName.style = "color:#ffeb3b;font-size:30px";
+	}
+}
+
+// Hàm kiểm tra điều kiện chọn ảnh giới tính
+imagesSex[0].onclick = function(){
+	imagesAvatar.src = "../session10/images/male.jpg";
+	userName.style = "color:#f44336"
+}
+
+imagesSex[1].onclick = function(){
+	imagesAvatar.src = "../session10/images/female.jpg";
+	userName.style = "color:#3f51b5"
+}
+
+
+
+
+
+},false)
